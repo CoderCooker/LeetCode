7a20984f088ec6b8255fc5e7da6c118635efba20
CoderCooker <jimin.susan.hu@gmail.com>
2017-06-02 07:22:43 -0700

    version 1

---
+++ e2e1.java
@@ -0,0 +1,11 @@
+public Class E2E1{
+
+  public void function(){
+    System.out.println(" work this out. ");
+  }
+  
+  public static void main(String[] args){
+    System.out.println(" Bites it tightly. ");
+  }
+
+}
